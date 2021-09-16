821cb3ede1a4133417e9837a190ae8572ae164e1
FelipeSDS <felipesilva@fiorde.com.br>
2021-09-15 22:55:06 -0300

    Inserindo input e function para pegar valores input

--- src/pages/Departamentos/Cadastrar/index.js
+++ src/pages/Departamentos/Cadastrar/index.js
@@ -4,15 +4,60 @@ import { Header } from "../../../components/Header";
 
 export default class CadastrarDepartamentos extends Component{
 
+    constructor(props){
+        super(props)
+
+        this.state = {
+            txNome : '',
+            txSobrenome: '',
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+      }
+
+    handleSubmit = (e) =>{
+        e.preventDefault()
+        console.log(this.state);
+    }
+
     render(){
+        const { txNome, txSobrenome } = this.state
         return(
             <>
                 <Header />
-                <div class="container">
-                    <div className="generic-div-style shadow p-3 mb-5 bg-body rounded">
-                        <div>
-                            <h1>Cadastrar Departamentos</h1>
-                        </div>
+                <div className="container">
+                    <div className="container shadow p-3 space-header">
+                        <form onSubmit={this.handleSubmit}>
+                            <div className="row">
+                                <div className="col">
+                                    <div className="form-group">
+                                        <label>Nome</label>
+                                        <input 
+                                            type="text" 
+                                            className="form-control" 
+                                            name="txNome" 
+                                            value={txNome}
+                                            onChange={this.handleChange}
+                                        />
+                                    </div>
+                                </div>
+                                <div className="col">
+                                    <div className="form-group">
+                                        <label>Sobrenome</label>
+                                        <input 
+                                            type="text" 
+                                            className="form-control" 
+                                            name="txSobrenome" 
+                                            value={txSobrenome}
+                                            onChange={this.handleChange}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <button type="submit" className="btn btn-primary">Salvar</button>
+                        </form>
                     </div>
                 </div>
             </>
